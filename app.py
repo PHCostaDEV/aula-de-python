2eb424dfa297db8a61fdf942b1d160fc833d480a
AlunoTI <AlunoTI@estacio.acad>
2026-04-30 09:06:17 -0300

    criar botão de quit

--- app.py
+++ app.py
@@ -4,8 +4,16 @@ class Application:
     def __init__(self, master=None):
         self.widget1 = Frame(master)
         self.widget1.pack()
-        self.msg = Label(self.widget1, text="Meu Primeiro wiget")
+
+        self.msg = Label(self.widget1, text="Meu Primeiro widget")
         self.msg.pack()
+
+        self.sair = Button(self.widget1)  # <-- criar o botão
+        self.sair["text"] = "Sair"
+        self.sair["font"] = ("Calibri", "10")
+        self.sair["width"] = 5
+        self.sair["command"] = self.widget1.quit  # <-- sem ()
+        self.sair.pack()
 
 root = Tk()
 Application(root)
